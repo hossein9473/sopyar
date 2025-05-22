440912a4ee201ce9d5c3b072a43534b3a7010d13
Hossein <mrhossein2012@gmail.com>
2025-05-22 13:06:46 +0330

    sabte darkhas page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,8 +4,8 @@ import Contact from "@/components/Contact";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Contact Page | Free Next.js Template for Startup and SaaS",
-  description: "This is Contact Page for Startup Nextjs Template",
+  title: "ثبت درخواست انگیزه نامه ، رزومه و توصیه‌نامه",
+  description: "ثبت سفارش انگیزه نامه ، رزومه و توصیه‌نامه",
   // other metadata
 };
 
@@ -13,8 +13,8 @@ const ContactPage = () => {
   return (
     <>
       <Breadcrumb
-        pageName="Contact Page"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+        pageName="ثبت درخواست"
+        description="برای ثبت درخواست انگیزه نامه ، رزومه و توصیه‌نامه داستان مسیر خود را برای ما بنویسید."
       />
 
       <Contact />
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -110,7 +110,7 @@ const Footer = () => {
             </div>
 
             <div dir= 'rtl' className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-2/12 xl:w-2/12">
-              <div className="mb-12 lg:mb-16">
+              <div>
                 <h2 className="mb-10 text-xl font-bold text-black dark:text-white">
                   لینک‌های مرتبط
                 </h2>
